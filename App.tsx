676b61b8ce417213caef70aee19d586a838aabbf
Dave Lee <ysangy@gmail.com>
2019-06-29 11:33:05 -0400

    layout testing

--- App.tsx
+++ App.tsx
@@ -11,9 +11,10 @@
 // import React, {Component} from 'react';
 import React from 'react';
 import { Component } from 'react';
-import {Platform, StyleSheet, Text, Image, TouchableOpacity, FlatList, View} from 'react-native';
+import { Platform, StyleSheet, Text, Image, TouchableOpacity, FlatList, View } from 'react-native';
 import data from './data.json';
-import  Icon from 'react-native-vector-icons/MaterialIcons'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import VideoItem from './components/videoItem'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -22,12 +23,34 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-interface Props {}
+interface Props { }
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-          <Text>Hello World</Text>
+        <View style={styles.navBar}>
+          <Image source={require('./images/logo.png')} style={{ width: 98, height: 22 }} />
+          <View style={styles.rightNav}>
+            <TouchableOpacity>
+              <Icon style={styles.navItem} name="search" size={25} />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Icon style={styles.navItem} name="account-circle" size={25} />
+            </TouchableOpacity>
+          </View>
+
+        </View>
+        <View style={styles.body}>
+          <FlatList
+            data={data.items}
+            renderItem={(video) => <VideoItem video={video.item} />}
+            keyExtractor={(item) => item.id}
+            ItemSeparatorComponent={() => <View style={{ height: 0.5, backgroundColor: '#E5E5E5' }} />}
+          />
+        </View>
+
+
+        <View style={styles.tabBar}><Text>tabBar</Text></View>
       </View>
     );
   }
@@ -36,18 +59,31 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+  },
+  navBar: {
+    height: 55,
+    backgroundColor: 'white',
+    elevation: 3,
+    paddingHorizontal: 15,
+    flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'space-between'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  rightNav: {
+    flexDirection: 'row'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  navItem: {
+    marginLeft: 25
   },
+  body: {
+    flex: 1
+  },
+  tabBar: {
+    backgroundColor: 'white',
+    height: 60,
+    borderTopWidth: 0.5,
+    borderColor: '#E5E5E5',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  }
 });
